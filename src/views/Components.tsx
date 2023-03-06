83c661d1bc063e83c52bd4e9cd27e3b1c989af98
Dylan Stockinger <pro.stockinger@gmail.com>
2023-03-06 19:07:27 +0100

    ⚡️ 💄

---
+++ src/views/Components.tsx
@@ -0,0 +1,9 @@
+import { SearchBar } from "../components/SearchBar";
+
+export const Components = () => {
+  return (
+    <div className="w-full h-screen bg-[#10141E]">
+      <SearchBar placeholder="Search for movies or TV series" />
+    </div>
+  );
+};
